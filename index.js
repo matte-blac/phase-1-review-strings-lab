68c44b5ff1d29c00b9de22e6a5726ec6cd929669
Matthew Bomett <bomettmat2@gmail.com>
2023-09-18 22:24:05 +0300

    completed message

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code in this file!
+
+const currentUser = "matt"
+const welcomeMessage = "Welcome to Flatbook, matt!"
+const excitedWelcomeMessage = "WELCOME TO FLATBOOK, MATT!"
+const shortGreeting = "Welcome, m!"
